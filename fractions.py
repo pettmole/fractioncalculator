ae9e37a4f70a4202997aa5421cc3be70960cc909
pettmole <84863024+pettmole@users.noreply.github.com>
2021-05-26 11:55:01 -0400

    Create fractions.py

---
+++ fractions.py
@@ -0,0 +1,23 @@
+# first fraction
+print("input numerator1")
+numerator1 = int(input())
+print(numerator1)
+print("input denominator1")
+denominator1 = int(input())
+print(denominator1)
+
+# second fraction
+print("input numerator2")
+numerator2 = int(input())
+print(numerator2)
+print("input denominator2")
+denominator2 = int(input())
+
+# answer variables
+numeratoranswer = (numerator1 * numerator2)
+denominatoranswer = (denominator1 * denominator2)
+
+# printing the answer
+print(numeratoranswer)
+print("--")
+print(denominatoranswer)
